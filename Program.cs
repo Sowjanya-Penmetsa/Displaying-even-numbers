3ee264ac81e62f2dc4634b2ecde01a868eeb2fee
unknown <penmetsa.sowjanya30@gmail.com>
2020-05-14 23:49:41 +0200

    my first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Display_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Numbers = new int[10];
+            int i;
+
+            Console.WriteLine("Enter 10 numbers ");
+
+            //Reading the numbers into the array
+
+            for (i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Displaying the numbers in the array
+
+            Console.WriteLine("The numbers in the array are:");
+            for(i = 0; i < Numbers.Length; i++)
+            Console.WriteLine(Numbers[i]);
+
+            //Displaying the even numbers in the array
+
+            Console.WriteLine("Following are the even numbers ");
+            for(i=0;i<Numbers.Length;i++)
+            {
+                if (Numbers[i] % 2 == 0)
+                    Console.WriteLine(Numbers[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
